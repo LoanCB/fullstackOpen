72279c39ee47107e348a08a0e5356bd742a033cd
Loan COURCHINOUX-BILLONNET <loan.courchinoux-billonnet@ipexia.com>
2023-03-13 15:02:57 +0100

    1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,32 +1,30 @@
 const Header = (props) => {
   return (
-    <>
-      <h1>{props.course}</h1>
-    </>
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercices}
+    </p>
   )
 }
 
 const Content = (props) => {
   return (
     <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1} exercices={props.exercises1} />
+      <Part part={props.part2} exercices={props.exercises2} />
+      <Part part={props.part3} exercices={props.exercises3} />
     </>
   )
 }
 
 const Total = (props) => {
   return (
-    <>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-    </>
+    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
   )
 }
 
