2a9d8cc976310aec416cad9d05ce3dfd199e52b5
Loan COURCHINOUX-BILLONNET <loan.courchinoux-billonnet@ipexia.com>
2023-03-15 14:43:45 +0100

    2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,32 +1,4 @@
-const Header = ({name}) => (
-  <h1>{name}</h1>
-)
-
-const Part = ({part}) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-)
-
-const Content = ({parts}) => (
-  <ul>
-    {parts.map(part =>
-      <li key={part.id}><Part part={part} /></li>
-    )}
-  </ul>
-)
-
-const Total = ({parts}) => (
-  <strong>total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</strong>
-)
-
-const Course = ({course}) => (
-  <>
-    <Header name={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts} />
-  </>
-)
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
