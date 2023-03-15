f27fb3472ef6c4dd239f117165e3de76c92effe1
Loan COURCHINOUX-BILLONNET <loan.courchinoux-billonnet@ipexia.com>
2023-03-15 15:37:32 +0100

    2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+const Numbers = ({persons}) => (
+  <>
+    <h2>Numbers</h2>
+    <ul>
+      {persons.map(person =>
+        <li key={person.name}>{person.name}</li>
+      )}
+    </ul>
+  </>
+)
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handlePersonChange = (event) => setNewName(event.target.value)
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({name: newName}))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handlePersonChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={addPerson}>add</button>
+        </div>
+      </form>
+      <Numbers persons={persons} />
+    </div>
+  )
+}
+
+export default App
