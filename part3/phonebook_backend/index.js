1219c979b8fd90bfb472aba17cee651aa021e567
Loan COURCHINOUX-BILLONNET <loan.courchinoux-billonnet@ipexia.com>
2023-03-17 17:01:43 +0100

    3.3

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -36,6 +36,16 @@ app.get('/api/info', (request, response) => {
         <p>${currentDate}</p>
     `)
 })
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
 
 
 app.use(express.json())
