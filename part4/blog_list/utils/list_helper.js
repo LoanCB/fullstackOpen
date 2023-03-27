020962a2ac1b2f1e9120605bde7e97b1bad6ff15
Loan COURCHINOUX-BILLONNET <loan.courchinoux-billonnet@ipexia.com>
2023-03-27 14:44:49 +0200

    4.5

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -6,6 +6,10 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, current) => sum + current.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((max, current) => max.likes > current.likes ? max : current)
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
