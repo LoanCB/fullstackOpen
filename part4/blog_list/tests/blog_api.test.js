7e98129e6b33d273cbbfe2be358716670f385b6b
Loan COURCHINOUX-BILLONNET <loan.courchinoux-billonnet@ipexia.com>
2023-03-28 11:13:12 +0200

    update 4.8

--- part4/blog_list/tests/blog_api.test.js
+++ part4/blog_list/tests/blog_api.test.js
@@ -1,8 +1,35 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const Blog = require('../models/blog')
 const app = require('../app')
 
 const api = supertest(app)
+const initialBlogs = [
+  {
+    title: `Django is the best framework <3`,
+    author: 'Loan CB',
+    url: 'https://www.djangoproject.com/',
+    likes: 17
+  },
+  {
+    title: `Foo`,
+    author: 'foo',
+    url: 'https://googe.com',
+    likes: 2
+  },
+  {
+    title: `HTML is easy`,
+    author: 'Loan CB',
+    url: 'https://googe.com',
+    likes: 8
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  for (i = 0; i < initialBlogs.length; i++)
+    await new Blog(initialBlogs[i]).save()
+})
 
 test('Blogs are returned as json', async () => {
   await api
@@ -13,7 +40,7 @@ test('Blogs are returned as json', async () => {
 
 test('Number of blogs', async () => {
   const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(11)
+  expect(response.body).toHaveLength(initialBlogs.length)
 })
 
 afterAll(async () => {
