6f811ff94b1e91a4eba95c7b94fb6c7812dd1683
Loan COURCHINOUX-BILLONNET <loan.courchinoux-billonnet@ipexia.com>
2023-04-12 10:30:03 +0200

    4.18

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -7,6 +7,7 @@ const { MONGODB_URI } = require('./utils/config')
 const app = express()
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const errorHandler = require('./utils/middleware')
 
 mongoose.set('strictQuery', false)
@@ -16,6 +17,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(errorHandler)
 
 module.exports = app
