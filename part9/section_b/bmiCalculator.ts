965431d77db830edef04b168beb3237fe0f0098e
Loan COURCHINOUX-BILLONNET <loan.courchinoux-billonnet@ipexia.com>
2023-04-19 14:23:09 +0200

    9.5

--- part9/section_b/bmiCalculator.ts
+++ part9/section_b/bmiCalculator.ts
@@ -17,17 +17,41 @@ const BmiParseArguments = (args: string[]): BmiEntries => {
   }
 }
 
-const calculateBmi = (height: number, weight: number): string => {
+export const HttpBmiParseArguments = (height: string, weight: string): BmiEntries => {
+  if (!isNaN(Number(height)) && !isNaN(Number(weight))) {
+    return {
+      height: Number(height),
+      weight: Number(weight)
+    }
+  } else {
+    throw new Error('malformatted parameters')
+  }
+}
+
+interface Response {
+  weight: number;
+  height: number;
+  bmi: string;
+}
+
+export const calculateBmi = (height: number, weight: number): Response => {
+  const response = {
+    weight: weight,
+    height: height,
+    bmi: ''
+  }
   height = height / 100;
   const result = weight / (height * height);
-  if (result < 16) return 'Underweight (Severe thinness)';
-  if (result <= 16.9) return 'Underweight (Moderate thinness)';
-  if (result <= 18.4) return 'Underweight (Mild thinness)';
-  if (result <= 24.9) return 'Normal range';
-  if (result <= 29.9) return 'Overweight (Pre-obese)';
-  if (result <= 34.9) return 'Obese (Class I)';
-  if (result <= 39.9) return 'Obese (Class II)';
-  return 'Obese (Class III)';
+
+  if (result < 16) response.bmi = 'Underweight (Severe thinness)';
+  else if (result <= 16.9) response.bmi = 'Underweight (Moderate thinness)';
+  else if (result <= 18.4) response.bmi = 'Underweight (Mild thinness)';
+  else if (result <= 24.9) response.bmi = 'Normal range';
+  else if (result <= 29.9) response.bmi = 'Overweight (Pre-obese)';
+  else if (result <= 34.9) response.bmi = 'Obese (Class I)';
+  else if (result <= 39.9) response.bmi = 'Obese (Class II)';
+  else response.bmi = 'Obese (Class III)';
+  return response;
 }
 
 try {
--- part9/section_b/index.ts
+++ part9/section_b/index.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import {calculateBmi} from "./bmiCalculator";
 const app = express();
 
 app.get('/hello', (_request, response) => {
     response.send('Hello Full Stack!');
 });
+app.get('/bmi', (req, res) => {
+    const {height, weight} = req.query
+    if (!isNaN(Number(height)) && !isNaN(Number(weight))) {
+        const bmi = calculateBmi(Number(height), Number(weight))
+        res.status(200).send(bmi)
+    } else {
+        res.status(400).send({error: 'malformatted parameters'})
+    }
+})
 
 const PORT = 3003;
 
