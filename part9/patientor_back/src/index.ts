3798c364d6ac31611de78a596fcf924945f74c88
Loan COURCHINOUX-BILLONNET <loan.courchinoux-billonnet@ipexia.com>
2023-04-20 11:56:02 +0200

    9.11

--- part9/patientor_back/src/index.ts
+++ part9/patientor_back/src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import diagnosesRouter from './routes/diagnoses';
+import patientsRouter from './routes/patients';
 const app = express();
-app.use(express.json());
 
-const PORT = 3001;
+app.use(express.json());
+app.use(cors());
 
 app.get('/api/ping', (_req, res) => {
     console.log('someone pinged here');
@@ -11,6 +13,9 @@ app.get('/api/ping', (_req, res) => {
 });
 
 app.use('/api/diagnoses', diagnosesRouter);
+app.use('/api/patients', patientsRouter);
+
+const PORT = 3001;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
---
+++ part9/patientor_back/src/services/patientServices.ts
@@ -0,0 +1,10 @@
+import patientData from '../../data/patients';
+import {PatientWithoutSsn} from "../types";
+
+const getEntries = (): PatientWithoutSsn[] => {
+    return patientData.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+        id, name, dateOfBirth, gender, occupation
+    }));
+};
+
+export default { getEntries };
