ee279b04899cf455e2353389ae233630e7c662fb
Loan COURCHINOUX-BILLONNET <loan.courchinoux-billonnet@ipexia.com>
2023-04-20 17:02:06 +0200

    9.14

---
+++ part9/my-app/src/components/Content.tsx
@@ -0,0 +1,23 @@
+import {CourseParts} from "../types";
+
+interface ContentProps {
+    courseParts: CourseParts[];
+}
+
+const Content = (props: ContentProps) => {
+    return (
+        <>
+            <p>
+                {props.courseParts[0].name} {props.courseParts[0].exerciseCount}
+            </p>
+            <p>
+                {props.courseParts[1].name} {props.courseParts[1].exerciseCount}
+            </p>
+            <p>
+                {props.courseParts[2].name} {props.courseParts[2].exerciseCount}
+            </p>
+        </>
+    );
+};
+
+export default Content;
